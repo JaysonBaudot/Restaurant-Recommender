734bd5a09c14257691330e4b731c9253a80c9f9a
Jayson Baudot <jaysonbaudot@gmail.com>
2022-06-30 13:04:26 -0500

    final

--- linkedlist.py
+++ linkedlist.py
@@ -22,9 +22,15 @@ class LinkedList:
     
     def remove_node(self, value_to_remove):
         current_node = self.head_node
-        if current_node.value == value_to_remove:
+        #test to see if .value works the same as .get_value()
+        if current_node.value == value_to_remove: 
             self.head_node = current_node.get_next_node()
         else:
             while current_node:
                 next_node = current_node.get_next_node()
-                
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                    current_node = None
+                else:
+                    current_node = next_node
+
